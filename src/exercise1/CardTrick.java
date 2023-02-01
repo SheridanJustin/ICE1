e919b85830d001e23e6a05817911b5787b55e4bf
SheridanJustin <122491029+SheridanJustin@users.noreply.github.com>
2023-02-01 18:19:12 -0500

    Update CardTrick.java
    Added printinfo about myself

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -77,19 +77,19 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Justin, no title needed");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- Familiarize myself with Java");
+        System.out.println("-- Master my first coding language!");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Weight Lifting");
+        System.out.println("-- Video Games");
+        System.out.println("-- Cardio");
+        System.out.println("-- Sports");
 
         System.out.println();
         
